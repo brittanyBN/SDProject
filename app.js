46041ed2556ffab9b18531eefc74518b418d3865
Brittany <brittany.bolling@aol.com>
2023-05-02 10:47:04 +0200

    add cyclic

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fileUpload = require('express-fileupload');
+dotenv = require('dotenv').config();
 
 
 const indexRouter = require('./routes/index');
--- routes/pictures.js
+++ routes/pictures.js
@@ -2,23 +2,53 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
-  const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
-  res.render('pictures', { pictures: pictures});
+router.get('/', async function (req, res, next) {
+  let params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/'
+  };
+  let allObjects = await s3.listObjects(params).promise();
+  let keys = allObjects?.Contents.map(x => x.Key)
+  const pictures = await Promise.all(keys.map(async (key) => {
+    let my_file = await s3.getObject({
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: key,
+    }).promise();
+    return {
+      src: Buffer.from(my_file.Body).toString('base64'),
+      name: key.split("/").pop()
+    }
+  }))
+  res.render('pictures', { pictures: pictures });
 });
 
 /* GET picture. */
-router.get('/:picture', function(req, res, next) {
+router.get('/:picture', async function (req, res, next) {
+  let params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: '/',
+    Prefix: 'public/'
+  };
+  let allObjects = await s3.listObjects(params).promise();
+  let keys = allObjects?.Contents.map(x => x.Key)
   let picture = req.params.picture;
   picture += '.JPG';
-    res.sendFile(path.join(__dirname, '../pictures/', picture));
+  res.sendFile(path.join(__dirname, '../pictures/', picture));
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const file = req.files.file;
-  fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
+  console.log(req.files);
+  await s3.putObject({
+    Body: file.data,
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + file.name,
+  }).promise()
   res.end();
 });
 
